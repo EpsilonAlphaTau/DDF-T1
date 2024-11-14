0abbafdc29de62b1a6cff96fc312d6fd79f0ac61
EpsilonAlphaTau <jchristophe.guerin@hotmail.fr>
2024-11-14 16:37:16 +0100

    enc

--- JS/INCLUDES/ecussontanneur.js
+++ JS/INCLUDES/ecussontanneur.js
@@ -1,1 +1,2 @@
-aCompleter('tanneur ecusson');
+readJS('C','',"On vous indique que c'est un écusson assez vieux. La personne qui le portait n'était probablement plus tanneur depuis longtemps.",'0',24, "Tannerie écusson", "Écusson de la Tannerie", 87);
+links(';');
