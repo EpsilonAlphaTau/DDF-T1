cbda148fe453df01f64a9172e4e333145bea4061
EpsilonAlphaTau <jchristophe.guerin@hotmail.fr>
2024-11-14 11:08:06 +0100

    def

--- JS/INCLUDES/adressedeeyugoubelanmissionordre.js
+++
@@ -1,2 +0,0 @@
-readJS('C','',"Les marins vous dessinent grossièrement un <i>plan</i>au dos de l'ordre de mission...",'16',147, 'Goubelan adresse ordre de mission', "Adresse de Goubelan sur l'ordre de mission", 105);
-links(';');
