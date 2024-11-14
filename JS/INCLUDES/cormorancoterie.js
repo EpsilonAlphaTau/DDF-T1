0e582037d6714bf228e2db47ddfc28942584270e
EpsilonAlphaTau <jchristophe.guerin@hotmail.fr>
2024-11-14 17:01:30 +0100

    errors

--- JS/INCLUDES/cormorancoterie.js
+++ JS/INCLUDES/cormorancoterie.js
@@ -1,1 +1,2 @@
-aCompleter('coterie cormoran');
+readJS('C','',"C'est manifestement un groupe très secret...",'3',31, "Coterie du cormoran", "Coterie du cormoran", 110);
+links(';');
--- JS/INCLUDES/coteriepie.js
+++ JS/INCLUDES/coteriepie.js
@@ -1,1 +1,2 @@
-aCompleter('coterie pie');
+readJS('C','',"C'est manifestement un groupe très secret...",'3',32, "Coterie de la pie", "Coterie de la pie", 111);
+links(';');
