8aa77b61fb0c31394ab99fda740cc4fd3fd037b7
EpsilonAlphaTau <jchristophe.guerin@hotmail.fr>
2024-11-15 16:47:16 +0100

    last

--- JS/INCLUDES/cormoranoouutttete.js
+++ JS/INCLUDES/cormoranoouutttete.js
@@ -1,2 +1,2 @@
-readJS('I','',"La tête s'évade à nouveau du lac, fait plusieurs cercle autour de vous, et s'échappe dans les brumes tandis que vous vous réveillez.",'28',227, "Tête cormoran", "Tête et cormoran ?", 193);
+readJS('I','',"La tête s'évade à nouveau du lac, décrit plusieurs cercles autour de vous, et s'échappe dans les brumes tandis que vous vous réveillez.",'28',227, "Tête cormoran", "Tête et cormoran ?", 193);
 links('');
