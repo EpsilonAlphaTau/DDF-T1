b12fdc1d807845407e417741fdaa7ba1389656fd
EpsilonAlphaTau <jchristophe.guerin@hotmail.fr>
2024-11-19 18:20:44 +0100

    baliran

--- JS/INCLUDES/balibranvalerin.js
+++
@@ -1,2 +0,0 @@
-readJS('C','',"C'est une personne très riche, qui semble habiter Phéonédura, dans le nord lointain.<br/><br/>Il semble être mêlé à toute cette affaire... Mais en quoi exactement ?",'0',65, "Valerin Balibran", "Valerin Balibran", 199);
-links('238');
---
+++ JS/INCLUDES/baliranpurge.js
@@ -0,0 +1,1 @@
+aCompleter('baliran purge');
